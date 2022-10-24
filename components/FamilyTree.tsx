166e77d86d88bf615d161473e52a2199a8339892
zawie <zawie@rice.edu>
2022-10-24 18:18:09 -0500

    fix style

--- components/FamilyTree.tsx
+++ components/FamilyTree.tsx
@@ -16,18 +16,20 @@ export type Scope = {
 
 function FamilyTree(props: FamilyTreeProps) {
     const { scope, doSearch} = props;
-    if (typeof window !== "undefined")
-        return <Tree label={StudentCardGroup(scope.parents, doSearch, false, "Advisors")}>
+    return <div style={{
+            overflowY:"auto"
+        }}>
+        <Tree label={StudentCardGroup(scope.parents, doSearch, false, "Advisors")}>
             <TreeNode key={scope.focus} label={StudentCardGroup([scope.focus], doSearch, true)}>
                 {scope.kids.map(kid =>
-                 <TreeNode key={kid} label={StudentCardGroup([kid], doSearch, false)}/>
+                    <TreeNode key={kid} label={StudentCardGroup([kid], doSearch, false)}/>
                 )}
             </TreeNode>
             {scope.siblings.map(sibling =>
-             <TreeNode key={sibling} label={StudentCardGroup([sibling], doSearch, false)}/>
+                <TreeNode key={sibling} label={StudentCardGroup([sibling], doSearch, false)}/>
             )}
-        </Tree>;
-    return <p> This is only renderable in the browser </p>
+        </Tree>
+    </div>
 }
 
 export default FamilyTree
