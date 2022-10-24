1c0a27ca341fcc6791e608861b75a55d3df7d9be
zawie <zawie@rice.edu>
2022-10-23 23:36:56 -0500

    working api :)

---
+++ pages/api/getFamily.ts
@@ -0,0 +1,124 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import type { StudentFamily} from "../model/types";
+
+import { google } from "googleapis";
+
+//Keys where here lol
+type ErrorResponse = {
+  error: string
+}
+
+type FamilySearchResponse = {
+    query: string,
+    homeFamilies: Family[],
+    advisingFamilies:Family[]
+}
+type Family = {
+    name: string,
+    parents: string[],
+    kids: string[],
+    year: string
+}
+
+type Row = {
+    name: string,
+    parents: string,
+    kids: string,
+    year: string
+}
+
+async function getFamilies(): Family[] {
+    const auth = new google.auth.JWT({
+        email: SERVICE_ACCOUNT_EMAIL,
+        key: SERVICE_ACCOUNT_PRIVATE_KEY,
+        scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+    })
+    const sheet = google.sheets("v4")
+    const sheetsResponse = await sheet.spreadsheets.get({
+        spreadsheetId: SHEET_ID,
+        ranges: ["Responses!A:H"],
+        includeGridData: true,
+        auth: auth,
+    });
+    const rowData: Array<any> = sheetsResponse.data.sheets[0].data[0].rowData
+    rowData.shift() // Removes headers
+    const rows: Row[] = rowData.map(r => {
+        const values: Array<string> = r.values.map(v => v.formattedValue) as Array<string>;
+        return {
+            name: values[2],
+            year: values[3],
+            parents: values[4],
+            kids: values[5],
+        };
+    });
+    return rows.map(r => {
+        return {
+            name: r.name,
+            year: r.year,
+            parents: r.parents.split(/,+/).map(s=> s.trim()),
+            kids: r.kids.split(/,+/).map(s=> s.trim())
+        };
+    })
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<FamilySearchResponse | ErrorResponse>
+) {
+
+    const {query}: {query? : string} = req.query
+
+    if (query == undefined) {
+        res.status(400).json({
+            error: "no query not specified"
+        });
+        return;
+    }
+
+    //Get all families from sheets
+    const families: Family[] = await getFamilies();
+
+    //TODO: Coalesce like familes
+    const homeFamilies = families.filter(f => f.kids.includes(query));
+    const advisingFamilies = families.filter(f => f.parents.includes(query));
+
+    res.status(200).json({
+        query, advisingFamilies, homeFamilies
+    })
+}
+
+
+// ;(async () => {
+//     const auth = new google.auth.JWT({
+//         email: SERVICE_ACCOUNT_EMAIL,
+//         key: SERVICE_ACCOUNT_PRIVATE_KEY,
+//         scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+//     })
+//     const sheet = google.sheets("v4")
+//     const sheetsResponse = await sheet.spreadsheets.get({
+//         spreadsheetId: SHEET_ID,
+//         ranges: ["Responses!A:H"],
+//         includeGridData: true,
+//         auth: auth,
+//     });
+//     const rowData: Array<any> = sheetsResponse.data.sheets[0].data[0].rowData as Array<any> ;
+//
+//     rowData.shift() // Removes headers
+//     rowData.forEach(r => {
+//         const values: Array<string | undefined> = r.values.map(v => v.formattedValue) as Array<string | undefined>;
+//         const row: Row = {
+//             timestamp: values[0],
+//             email: values[1],
+//             name: values[2],
+//             netid: values[3],
+//             college: values[4],
+//             parents: values[5],
+//             siblings: values[6],
+//             kids: values[7],
+//         }
+//
+//         console.log("netid:\t"+row.netid);
+//     })
+// })()
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,47 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import dynamic from "next/dynamic";
 
-import styles from '../styles/Home.module.css'
+import { demoFamily } from "./model/types";
 
-import { demoFamily } from "./model/types.ts";
-
-import  { PageHeader, Button, Typography } from 'antd';
+import  { PageHeader, Button } from 'antd';
 import { UserAddOutlined} from "@ant-design/icons";
+import { StudentFamily} from "./model/types";
+import { useState} from "react";
 
 const Home: NextPage = () => {
-    const FamilyTree = dynamic(
-            () => import("./components/FamilyTree"), // replace '@components/map' with your component's location
-            { loading: () => <div>Loading...</div>, ssr: false} // This line is important. It's what prevents server-side render
-            );
 
-    return(<>
+    const [family, setFamily]: [StudentFamily | undefined, any] = useState(undefined);
+
+
+    const FamilyTree = dynamic(
+        () => import("./components/FamilyTree"),
+        { loading: () => <div>Loading...</div>, ssr: false}
+    );
+
+
+    const callAPI = async () => {
+        try {
+            const res = await fetch(
+                `http://localhost:3000/api/getFamily`
+                );
+            const data = await res.json();
+            console.log(data);
+            setFamily(data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    
+    if (family == undefined)
+        return <div>Loading...
+
+            <Button onClick={() => callAPI()}>
+                Call API
+            </Button>
+
+    </div>;
+    else
+        return <>
             <PageHeader
                 title="O-Week Family Tree"
                 extra={[
@@ -25,11 +50,14 @@ const Home: NextPage = () => {
                     </Button>
                 ]}
             />
+            <Button onClick={() => callAPI()}>
+                Call API
+            </Button>
+
             <div className="App">
-                <FamilyTree family={demoFamily}/>
+                <FamilyTree family={family}/>
             </div>
-        </>
-    );
+        </>;
 }
 
 export default Home
