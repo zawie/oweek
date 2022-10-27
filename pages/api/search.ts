ef597bf7af9e8eaaf7b142df7acfe08b03b66e44
zawie <zawie@rice.edu>
2022-10-27 16:46:03 -0500

    newphew and grandkids api

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -8,10 +8,12 @@ type ErrorResponse = {
 }
 
 export type SearchResult = {
-    queryMade: string | undefined,
+    queryMade?: string,
     focusName: string,
     homeFamilies: Family[],
-    advisingFamilies: Family[],
+    newphewFamilies: Family[],
+    parentFamilies: Family[],
+    grandFamilies: Family[],
 }
 
 export default async function handler(
@@ -29,12 +31,30 @@ export default async function handler(
 
     //Find all families associated with this name
     const homeFamilies = families.filter(f => f.kids.includes(name));
-    const advisingFamilies = families.filter(f => f.parents.includes(name));
+    const parentFamilies = families.filter(f => f.parents.includes(name));
 
+    let kids: string[] = [];
+    parentFamilies.forEach(family => {
+      kids = kids.concat(family.kids);
+    });
+    const grandFamilies = families.filter(f => 
+      f.parents.some(p => kids.includes(p))
+    );
+
+    let siblings: string[] = [];
+    homeFamilies.forEach(family => {
+      kids = kids.concat(family.kids);
+    });
+    const newphewFamilies = families.filter(f => 
+      f.parents.some(p => siblings.includes(p))
+    );
+    
     res.status(200).json({
         focusName: name, 
         queryMade: query,
-        advisingFamilies, 
-        homeFamilies
+        homeFamilies,
+        newphewFamilies,
+        parentFamilies, 
+        grandFamilies
     })
 }
