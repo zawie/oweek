6860ba0600b1cc29fba827b0ad23efbe5f335e52
zawie <zawie@rice.edu>
2022-10-28 22:02:08 -0500

    reinstate dropdown

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -78,7 +78,7 @@ export default function SearchBar({ disabled, doSearch } : SearchBarProps) {
         > 
             <SyncOutlined />Random 
         </Button>  
-        {/* <Dropdown overlay={menu} open={showSuggestions}> */}
+        <Dropdown overlay={menu} open={showSuggestions}>
             <Search
                 disabled={disabled}
                 size="large"
@@ -104,7 +104,7 @@ export default function SearchBar({ disabled, doSearch } : SearchBarProps) {
                 }}
                 prefix={<UserOutlined />}
             />
-        {/* </Dropdown> */}
+        </Dropdown>
     </div>
 }
   
