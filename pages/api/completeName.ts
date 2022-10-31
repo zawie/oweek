5836266d13ba65a18d2a0494328aeddb8a7f9a63
zawie <zawie@rice.edu>
2022-10-31 11:05:11 -0500

    remove edge config

--- pages/api/completeName.ts
+++ pages/api/completeName.ts
@@ -17,10 +17,6 @@ export type CompleteNameResult = {
 let cachedNames: Trie | undefined = undefined;
 export const minPartialLength = 1;
 
-export const config = {
-    runtime: 'experimental-edge',
-};
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<CompleteNameResult | ErrorResponse>
