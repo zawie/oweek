80de9e5da5d6b676f8b34bb80bfdd5e85a14fa3b
zawie <zawie@rice.edu>
2022-10-31 12:10:24 -0500

    refactor _app.tsx and add github link

---
+++ components/PageHeader.tsx
@@ -0,0 +1,28 @@
+import { Typography, Button } from 'antd';
+const { Title } = Typography;
+import { UserAddOutlined } from '@ant-design/icons';
+
+type PageHeaderProps = {
+}
+
+export default function PageHeader(props: PageHeaderProps) {
+    return <div className="PageHeader">
+        <div style={{paddingTop:10}} >
+            <Title style={{
+                fontSize: 32,
+            }}> 🌳 O-Week Genealogy </Title>
+        </div>
+        <div style={{
+            height:"100%",
+            marginRight: 25
+        }}>
+            <Button type="link"
+                href="https://forms.gle/hUfXkadg8Z8L5Bt98"
+                size="large"
+                style={{
+                    fontSize: 16,
+                    minWidth: 128,
+            }}> <UserAddOutlined/> Add a family </Button>
+        </div>
+    </div>
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,53 +2,24 @@ import 'antd/dist/antd.css';
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
-import { Typography, Button} from 'antd'
-import { UserAddOutlined } from "@ant-design/icons";
+import { Typography} from 'antd'
 import { Analytics } from '@vercel/analytics/react';
+import PageHeader from '../components/PageHeader';
+import { GithubFilled } from '@ant-design/icons';
 
-const { Title, Text } = Typography;
+const { Text } = Typography;
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <div style={{backgroundColor: "whitesmoke", minHeight:"100vh", minWidth:"100vw"}}>
-        <div style={{
-            backgroundColor: "white",
-            width: "100vw",
-            paddingLeft: 16,
-            display:"flex",
-            alignItems: "center",
-            justifyContent: "space-between"
-        }}>
-            <div style={{paddingTop:10 }}>
-                <Title style={{
-                    fontSize: 32,
-                }}> 🌳 O-Week Genealogy </Title>
-            </div>
-            <div style={{
-                height:"100%",
-                marginRight: 25
-            }}>
-                <Button type="link"
-                    href="https://forms.gle/hUfXkadg8Z8L5Bt98"
-                    size="large"
-                    style={{
-                        fontSize: 16,
-                        minWidth: 128,
-                }}> <UserAddOutlined/> Add a family </Button>
-            </div>
-        </div>
+    return <div className="App">
+        <Analytics/>
+        <PageHeader/>
         <br/>
         <Component {...pageProps}/>
-        <div  style={{
-            width: "100vw",
-            paddingTop: 32,
-            paddingBottom: 32,
-            display:"flex",
-            alignItems: "center",
-            justifyContent: "center"
-        }}>
+        <br/>
+        <div className='Footer'>
             <Text> Made with love by the best college, <a href="https://www.browncollege.org">Brown College</a>. BSWB! {"🚀"} </Text>
+            <Text> Want to contribute? Make a pull request to the <a href="https://github.com/zawie/oweek-genealogy">git repository <GithubFilled/></a> </Text>
         </div>
-        <Analytics />
   </div>
 }
 
