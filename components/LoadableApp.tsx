2c8e4f10759cc1f6b3ca9e06f09120fde0075ed9
zawie <zawie@rice.edu>
2023-08-19 13:02:09 -0700

    Reduce First Load JS (API 150 kb -> 80kb)

---
+++ components/LoadableApp.tsx
@@ -0,0 +1,40 @@
+import type { AppProps } from 'next/app'
+import { Typography } from 'antd'
+import { Analytics } from '@vercel/analytics/react';
+import PageHeader from '../components/PageHeader';
+import { GithubFilled } from '@ant-design/icons';
+import Head from 'next/head';
+
+const { Text } = Typography;
+
+export const LoadableApp = ({ Component, pageProps }: AppProps) => {
+    return <div className="App">
+    <Head>
+        <title>
+            Rice O-Week Tree
+        </title>
+        <meta
+            name="description"
+            content="O-Week geneology site for Rice University orientation. Explore advisor and new student relationships in the Rice family tree!"
+            key="desc"
+        />
+        <meta 
+            name="author" 
+            content="Adam Zawierucha"
+            key="auth"
+        />
+        <meta name="keywords" 
+              content="O-Week, Oweek, Rice, Rice University, geneology, family tree"
+        />
+    </Head>
+  
+    <Analytics/>
+    <PageHeader/>
+    <Component {...pageProps} style={{width:"100%"}}/>
+    <br/>
+    <div className='Footer'>
+        <Text type="secondary"> Made with love by the best college, <a href="https://www.browncollege.org">Brown College</a>.</Text>
+        <Text type="secondary"> This is an open source project on <a href="https://github.com/zawie/oweek.org"> GitHub <GithubFilled/></a></Text>
+    </div>
+</div>
+}
