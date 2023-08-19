858db5500776023c1e50be3d0405d07f5466ef87
zawie <zawie@rice.edu>
2023-08-19 12:55:41 -0700

    build analyzer

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withPlugins([
+  [withBundleAnalyzer]
+])
+
 module.exports = nextConfig
