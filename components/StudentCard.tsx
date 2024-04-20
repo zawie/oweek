9a7ed4d8b2db05e92d71c81ca455ea5e0e5fe0f3
zawie <zawie@rice.edu>
2024-04-20 12:45:15 -0500

    Adds new owl avatars 🦉

--- components/StudentCard.tsx
+++ components/StudentCard.tsx
@@ -1,34 +1,36 @@
 import { Card, Typography} from 'antd';
 import Image from 'next/image'
 
-import owl1 from "../assets/owls/owl1.png"
-import owl2 from "../assets/owls/owl2.png"
-import owl3 from "../assets/owls/owl3.png"
-import owl4 from "../assets/owls/owl4.png"
-import owl5 from "../assets/owls/owl5.png"
-import owl6 from "../assets/owls/owl6.png"
-import owl7 from "../assets/owls/owl7.png"
-import owl8 from "../assets/owls/owl8.png"
-import owl9 from "../assets/owls/owl9.png"
-import owl10 from "../assets/owls/owl10.png"
-import owl11 from "../assets/owls/owl11.png"
-import owl12 from "../assets/owls/owl12.png"
-import owl13 from "../assets/owls/owl13.png"
-import owl14 from "../assets/owls/owl14.png"
-import owl15 from "../assets/owls/owl15.png"
-import owl16 from "../assets/owls/owl16.png"
-import owl17 from "../assets/owls/owl17.png"
-import owl18 from "../assets/owls/owl18.png"
-import owl19 from "../assets/owls/owl19.png"
-import owl20 from "../assets/owls/owl20.png"
-import owl21 from "../assets/owls/owl21.png"
-import owl22 from "../assets/owls/owl22.png"
-import owl23 from "../assets/owls/owl23.png"
+import owl1 from "../assets/owls/owl1.jpg"
+import owl2 from "../assets/owls/owl2.jpg"
+import owl3 from "../assets/owls/owl3.jpg"
+import owl4 from "../assets/owls/owl4.jpg"
+import owl5 from "../assets/owls/owl5.jpg"
+import owl6 from "../assets/owls/owl6.jpg"
+import owl7 from "../assets/owls/owl7.jpg"
+import owl8 from "../assets/owls/owl8.jpg"
+import owl9 from "../assets/owls/owl9.jpg"
+import owl10 from "../assets/owls/owl10.jpg"
+import owl11 from "../assets/owls/owl11.jpg"
+import owl12 from "../assets/owls/owl12.jpg"
+import owl13 from "../assets/owls/owl13.jpg"
+import owl14 from "../assets/owls/owl14.jpg"
+import owl15 from "../assets/owls/owl15.jpg"
+import owl16 from "../assets/owls/owl16.jpg"
+import owl17 from "../assets/owls/owl17.jpg"
+import owl18 from "../assets/owls/owl18.jpg"
+import owl19 from "../assets/owls/owl19.jpg"
+import owl20 from "../assets/owls/owl20.jpg"
+import owl21 from "../assets/owls/owl21.jpg"
+import owl22 from "../assets/owls/owl22.jpg"
+import owl23 from "../assets/owls/owl23.jpg"
+import owl24 from "../assets/owls/owl24.jpg"
+import owl25 from "../assets/owls/owl25.jpg"
 
 const { Text } = Typography;
 const { Meta } = Card;
 
-const owls = [owl1, owl2, owl3, owl4, owl5, owl6, owl7, owl8, owl9, owl10, owl11, owl12, owl13, owl14, owl15, owl16, owl17, owl18, owl19, owl20, owl21, owl22, owl23];
+const owls = [owl1, owl2, owl3, owl4, owl5, owl6, owl7, owl8, owl9, owl10, owl11, owl12, owl13, owl14, owl15, owl16, owl17, owl18, owl19, owl20, owl21, owl22, owl23, owl24, owl25];
 
 const hash = function(str: string): number {
     let hash: number = 0;
