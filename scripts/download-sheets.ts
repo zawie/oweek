6626f7b75070c33bb844044d75c4f4cd655a036a
zawie <zawie@rice.edu>
2024-04-23 22:53:06 -0700

    Tidy download-sheets ⬇️

--- scripts/download-sheets.ts
+++ scripts/download-sheets.ts
@@ -3,16 +3,12 @@ import { fetchFamilies } from "../helper/family";
 
 dotenv.config({path:'.env.local'})
 
-console.log("Importing families from Google Sheets...")
-
-//Write 'Hello, world!' to file namd hello.txt
 import * as fs from 'fs';
 
 // Use first argument as output file path
 const args = process.argv.slice(2)
 const outputFile = args[0]
 
-// Clear file
 fs.writeFileSync(outputFile, '', 'utf8');
 
 // Fetch families and write to file in .njson format
@@ -21,3 +17,5 @@ fetchFamilies().then(families => {
     .map(f => JSON.stringify(f))
     .map(s => fs.appendFileSync(outputFile, s + '\n', 'utf8'))
 })
+
+console.log(`Downloaded families to ${+ outputFile} from Google sheets! ⬇️`)
