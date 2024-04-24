9fd41fcd265b4f02031c16852ec48ef494695192
zawie <zawie@rice.edu>
2024-04-24 14:46:40 -0700

    Add issue comment 💬

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // https://github.com/ant-design/ant-design/issues/46053
   transpilePackages: [ "antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table" ],
 }
 
