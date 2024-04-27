6043060df40d2cf6aaa50239fdffb4dacafe49d0
Adam Zawierucha <adzawie@gmail.com>
2024-04-27 16:39:04 +0000

    Add speed insights 🚀

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,17 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import dynamic from 'next/dynamic';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const LoadableApp = dynamic(() =>
   import("../components/LoadableApp").then((m) => m.LoadableApp),
 );
 
 function MyApp(props: AppProps) {
-    return  <LoadableApp {... props}/>
+    return <>
+      <LoadableApp {... props}/>
+      <SpeedInsights />
+    </>
 }
 
 export default MyApp
